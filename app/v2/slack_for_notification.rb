02017d46fdb8a777ff67fdcdada7320ba84dfaca
r-ume <baymax.blackandwhite@gmail.com>
2018-02-05 22:48:38 +0900

    added sends_no_guidance_notifications method

--- app/v2/slack_for_notification.rb
+++ app/v2/slack_for_notification.rb
@@ -6,33 +6,26 @@ require_relative './calendar_item'
 # Slack
 class SlackForNotification
 
-  STARTER_NOTIFICATION      = '明日のシフトはこちら！'
-  HOPING_REACTION_STATEMENT = '`こちらにメンションがついている方は今日23時までに必ず本通知にリアクション` をお願いします!'
-  NO_SHIFT_STATEMENT        = '明日のシフトはありません！'
-  BIRTHDAY_STATEMENT        = 'そして、なんと明日誕生日のメンターが!!! そのメンターは...!!!'
+  GUIDANCE_STATEMENT                 = '明日ガイダンスがあるメンターはこちら！'
+  NO_GUIDANCE_STATEMENT              = '明日のガイダンスはありません！'
+  BIRTHDAY_STATEMENT                 = 'そして、なんと明日誕生日のメンターが!!! そのメンターは...!!!'
   CELERATION_ENCOURAGEMENT_STATEMENT = '明日会ったときに、「誕生日おめでとう」と言おう！'
 
-  @notifier = SlackNotifier.notifier
-
   class << self
-    def sends_starter_notification
-      SlackNotifier.post(text: STARTER_NOTIFICATION)
-      SlackNotifier.post(text: HOPING_REACTION_STATEMENT)
-    end
-
-    def sends_shift_notification(mention:, calendar_name:, start_time:)
-      notification = "<#{mention}> : #{calendar_name} : #{start_time}"
-      SlackNotifier.post(text: notification)
-    end
-
     def sends_birthday_notification(mention:, calendar_name:)
       SlackNotifier.post(text: "#{BIRTHDAY_STATEMENT}")
       SlackNotifier.post(text: "<#{mention}>#{calendar_name}　:tada: :tada:")
       SlackNotifier.post(text: CELERATION_ENCOURAGEMENT_STATEMENT)
     end
 
-    def sends_no_shift_notification
-      SlackNotifier.post(text: NO_SHIFT_STATEMENT)
+    def sends_guidance_notification(mention:, calendar_name:, start_time:)
+      SlackNotifier.post(text: GUIDANCE_STATEMENT)
+      SlackNotifier.post(text: "<#{mention}>#{calendar_name}")
+
+    end
+
+    def sends_no_guidance_notification
+      SlackNotifier.post(text: NO_GUIDANCE_STATEMENT)
     end
   end
 end
