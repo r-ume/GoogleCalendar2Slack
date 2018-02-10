9ffb0e0852fcaa8ad241a12cdcb0a14cf8199af3
r-ume <baymax.blackandwhite@gmail.com>
2018-02-10 15:17:44 +0900

    changed cron job time

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 env :PATH, ENV['PATH']
 set :output, { standard: 'cron/standard.log', error: 'cron/error.log' }
 
-every 1.day, at: '10:00 pm' do
+every 1.day, at: '09:00 pm' do
   command 'cd /Users/umeki/Desktop/Ruby/NotificationOnSlack && sh cron_script.sh'
 end
